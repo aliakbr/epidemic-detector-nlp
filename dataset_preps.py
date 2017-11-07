6b50a084bd41bcf25d2430c779102839e2d56bfd
Ali Akbar <aliraal12@gmail.com>
2017-11-07 08:39:19 +0700

    Edit data preps script

--- dataset_preps.py
+++ dataset_preps.py
@@ -9,12 +9,12 @@ import numpy as np
 import pandas as pd
 
 
-def get_tweets(root_dir):
+def get_tweets(root_dir, keyword):
     all_tweets_id = []
     all_tweets_class = []
     tweets_file = [os.path.join(root_dir, f) for f in os.listdir(root_dir)]
     for d in tweets_file:
-        if d.endswith('.txt'):
+        if d.endswith('.txt') and keyword.lower() in d.lower():
             tweets_file = open(d)
             for l in tweets_file:
                 value = l.strip('\n').split('\t')
@@ -24,36 +24,40 @@ def get_tweets(root_dir):
                 
                 
 
-# get all tweets
+# get tweets from file
+root_dir = 'flu_annotations'
+keyword = 'awareness'
+all_tweets_id, all_tweets_class = get_tweets(root_dir, keyword)
+
+# get tweets
+import tweepy
 CONSUMER_KEY = 'YkzeiWTXY8kWpjpKa0kPrTYYd'
 CONSUMER_SECRET = 'pp102MEm5nsGni6qJzuSaloAMjWiBMVhC5IbyilffLCkZN0rT9'
 ACCESS_TOKEN = '2574387926-bwfPiD9PwR1DLbcZaCnqfCdyRZJbbhPlpEFsax2'
 ACCESS_TOKEN_SECRET = 'YpgPlVlEhtokcLRVdm67IqGCOde0YDMqlSqwTLqM6F353'
 
-root_dir = 'flu_annotations'
-all_tweets_id, all_tweets_class = get_tweets(root_dir)
-import tweepy
-#api = twitter.Api(consumer_key=[CONSUMER_KEY],
-#                  consumer_secret=[CONSUMER_SECRET],
-#                  access_token_key=[ACCESS_TOKEN],
-#                  access_token_secret=[ACCESS_TOKEN_SECRET])
-
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
 api = tweepy.API(auth)
 
 all_tweets_status = []
+count = 0
+limit = 800
 for tweet_id in all_tweets_id:
     try:
         status = api.get_status(tweet_id)
+        count += 1
+        print (count)
+        if count == 800:
+            break
+        all_tweets_status.append(status.text)
     except:
-        continue
-    all_tweets_status.append(status.text)
+        all_tweets_status.append('')
 
 # Save Tweet
+filename = 'dataset_'+keyword+'.csv'
 df = pd.DataFrame()
-df['id'] = np.asarray(all_tweets_id)
-df['tweet'] = np.asarray(all_tweets_status)
-df['class'] = np.asarray(all_tweets_class)
+df['id'] = all_tweets_id
+df['tweet'] = all_tweets_status
+df['class'] = all_tweets_class
 df.to_csv('dataset.csv')
