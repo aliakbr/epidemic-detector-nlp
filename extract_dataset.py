ae22d2ac172dc4b84dbb6bfea7e71ea41bfd073b
mgumilang <muhammadgumilang11@gmail.com>
2017-11-23 08:09:23 +0700

    Add preprocess to dataset extraction

--- extract_dataset.py
+++ extract_dataset.py
@@ -2,8 +2,25 @@ import preprocess
 import pandas as pd
 
 filename = 'dataset_related.csv'
+output = 'data_related_extracted.txt'
 
 df = pd.read_csv(filename)
 df_extract = df.loc[:, ['tweet', 'class']]
 df_extract = df_extract.dropna()
 df_extract = df_extract.drop_duplicates()
+
+tweets = df_extract.tweet.values.tolist()
+classes = df_extract['class'].values.tolist()
+
+# Preprocess
+for i in range(len(tweets)):
+    tweets[i] = tweets[i].replace('\n', ' ')
+    tweets[i] = preprocess.preprocess(tweets[i])
+    
+s = []
+for tweet,cl in zip(tweets,classes):
+    s.append(tweet + '\t' + str(int(cl)) + '\n')
+
+with open(output, 'wb') as f:
+    for x in s:
+        f.write(x.encode('utf-8'))
